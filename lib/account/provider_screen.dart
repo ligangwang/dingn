64d258e67878d64a5f2f645308441bbe0a43de84
Ligang Wang <ligangwangs@gmail.com>
2020-01-11 21:57:33 -0500

    fix end of playing cards

--- lib/account/provider_screen.dart
+++ lib/account/provider_screen.dart
@@ -45,7 +45,7 @@ class MainScreen extends StatelessWidget {
                     ),
                   ),
                   SizedBox(width: 5),
-                  Text('v1.2.8',
+                  Text('v1.2.9',
                     style: TextStyle(color: Colors.grey, fontSize: AppTheme.fontSizeTiny)),
                 ]
               )
